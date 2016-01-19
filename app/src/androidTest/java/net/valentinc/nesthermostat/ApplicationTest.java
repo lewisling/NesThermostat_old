3dcf102045670e720457dbeb0240682af5984a63
Valentin C <valentindu64@hotmail.fr>
2016-01-19 18:57:34 +0100

    Start refractor by using ssh instead of php

--- app/src/androidTest/java/net/valentinc/nesthermostat/ApplicationTest.java
+++ app/src/androidTest/java/net/valentinc/nesthermostat/ApplicationTest.java
@@ -3,11 +3,44 @@ package net.valentinc.nesthermostat;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
+import net.valentinc.ssh.SSHManager;
+
+import org.junit.Test;
+
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Test
+    public void testSendCommand() {
+        System.out.println("sendCommand");
+
+        /**
+         * YOU MUST CHANGE THE FOLLOWING
+         * FILE_NAME: A FILE IN THE DIRECTORY
+         * USER: LOGIN USER NAME
+         * PASSWORD: PASSWORD FOR THAT USER
+         * HOST: IP ADDRESS OF THE SSH SERVER
+         **/
+        String command = "ls FILE_NAME";
+        String userName = "USER";
+        String password = "PASSWORD";
+        String connectionIP = "HOST";
+        SSHManager instance = new SSHManager(userName, password, connectionIP, "");
+        String errorMessage = instance.connect();
+
+        if (errorMessage != null) {
+            System.out.println(errorMessage);
+            fail();
+        }
+
+        String expResult = "FILE_NAME\n";
+        // call sendCommand for each command and the output
+        //(without prompts) is returned
+        String result = instance.sendCommand(command);
+        // close only after all commands are sent
+        instance.close();
+        assertEquals(expResult, result);
     }
 }
